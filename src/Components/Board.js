0b6c761b2e4a2f2a42d1b3533f8dbf7c81e210f1
Abhishek012kumarsingh <abhishekkumar012singh@gmail.com>
2022-10-05 17:51:40 +0530

    patch 1

---
+++ src/Components/Board.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Square from "./Square";
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        // to maintain each box
+      squares: Array(9).fill(null),
+    };
+  }
+// Square Click Event handler
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares:squares});
+  }
+//   Square component with states to handle click and show/maitain values 
+  renderSquare(i) {
+    return (
+    <Square 
+    value={this.state.squares[i]}
+    onClick={()=> this.handleClick(i)} />
+    );
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
